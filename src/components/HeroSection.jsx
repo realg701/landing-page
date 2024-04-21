24732ce6b22b66287ecf80f1b6e3643bf36b5e6b
Abdul Haseeb <125727302+realg701@users.noreply.github.com>
2024-04-22 00:54:19 +0500

    add: finalized

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -6,14 +6,13 @@ const HeroSection = () => {
       <div className="hero-content">
         <h1>YOUR FEET DESERVE THE BEST</h1>
         <p>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis
-          quisquam odio hic! Delectus quasi repellat distinctio architecto ullam
-          ipsa, mollitia vero numquam sunt minima adipisci, ipsam, molestiae
-          totam ad nobis.
+          YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR
+          SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR
+          SHOES.
         </p>
         <div className="hero-btn">
           <button>Shop Now</button>
-          <button>Category</button>
+          <button className="second-btn">Category</button>
         </div>
         <div className="shop">
           <p>Also Available On</p>
@@ -39,28 +38,79 @@ export default HeroSection;
 const MainContainer = styled.div`
   display: flex;
   max-width: 1280px;
-  margin: 70px auto;
-  height: calc(100vh - 70);
+  height: calc(100vh - 100px);
+  margin: 50px auto;
+  padding: 0 1.5em;
 
-  h1 {
-    font-size: 6em;
-    font-weight: 800;
-    line-height: 1em;
+  .hero-content {
+    display: flex;
+    flex-direction: column;
+    gap: 1em;
+    h1 {
+      font-size: 6em;
+      font-weight: 800;
+      line-height: 1em;
+    }
+    p {
+      max-width: 400px;
+      font-weight: 600;
+    }
+
+    .hero-btn {
+      display: flex;
+      gap: 2em;
+
+      .second-btn {
+        background: white;
+        color: #1a1a1a;
+      }
+
+      @media (prefers-color-scheme: light) {
+        button {
+          color: white;
+          background-color: #1a1a1a;
+        }
+        .second-btn {
+          border: 1px solid #1a1a1a;
+        }
+      }
+    }
+
+    .shop {
+      p {
+        font-weight: unset;
+        font-size: 14px;
+      }
+    }
+
+    .brand-icons {
+      display: flex;
+      gap: 1em;
+
+      button {
+        background: white;
+        display: flex;
+        align-items: center;
+        padding: 0.5em;
+
+        img {
+          width: 60px;
+          height: 30px;
+          object-fit: contain;
+        }
+      }
+    }
+
+    @media (prefers-color-scheme: light) {
+      button {
+        border: 1px solid #1a1a1a;
+      }
+    }
   }
 
-  .brand-icons {
-    display: flex;
-    button {
-      display: flex;
-      align-items: center;
-      margin: 5px;
-      padding: 0.5em 1em;
-
-      img {
-        width: 80px;
-        height: 40px;
-        object-fit: contain;
-      }
+  .hero-image {
+    img {
+      width: 140%;
     }
   }
 `;
